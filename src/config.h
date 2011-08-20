3d413b09facfb96da0dda4d22587d196e4733047
moetunes <moetunes111@gmail.com>
2011-08-20 19:07:32 +1000

    Updated files to include mention of catwm

--- src/config.h
+++ src/config.h
@@ -1,5 +1,6 @@
  /* config.h for DMiniWM.c [ 7 ]
  *
+ *  Started from catwm with many thanks!
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
