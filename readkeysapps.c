015b4e5ae0f650f6e171693dbc1061cc5cdb2805
moetunes <moetunes111@gmail.com>
2012-09-22 09:46:18 +1000

    Added AltSuper for Mod key combo

--- readkeysapps.c
+++ readkeysapps.c
@@ -40,6 +40,7 @@ void read_keys_file() {
                 else if(strncmp(dummy3, "Super", 5) == 0) keys[keycount].mod = Mod4Mask;
                 else if(strncmp(dummy3, "CtrlSuper", 9) == 0) keys[keycount].mod = Mod4Mask|ControlMask;
                 else if(strncmp(dummy3, "ShftSuper", 9) == 0) keys[keycount].mod = Mod4Mask|ShiftMask;
+                else if(strncmp(dummy3, "AltSuper", 8) == 0) keys[keycount].mod = Mod1Mask|Mod4Mask;
                 else continue;
                 keys[keycount].keysym = strsep(&dummy2, ";");
                 dummy3 = strsep(&dummy2, ";");
