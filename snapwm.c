601906cc50e02a07292975a34da0c3305c3de929
moetunes <moetunes111@gmail.com>
2012-12-05 17:16:44 +1000

    derp

--- snapwm.c
+++ snapwm.c
@@ -1112,14 +1112,15 @@ void buttonpress(XEvent *e) {
             return;
         }
     // change focus with LMB
-    unsigned int cds = desktops[cd].screen, tmp = current_desktop;
+    unsigned int cds = desktops[cd].screen;
     if(clicktofocus == 0 && ev->button == Button1)
+        save_desktop(cd);
         for(i=cds;i<cds+num_screens;++i) {
             select_desktop(view[i%num_screens].cd);
             for(c=head;c;c=c->next) {
                 if(ev->window == c->win) {
                     Arg a = {.i = current_desktop};
-                    select_desktop(tmp);
+                    select_desktop(cd);
                     change_desktop(a);
                     current = c;
                     update_current();
